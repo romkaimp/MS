be08022a3436be58e4c4699f68d118a1331b4527
Roman <roman.150704@mail.ru>
2025-03-02 22:44:08 +0300

    task done

--- Stochastic_processes/graph_tools.py
+++ Stochastic_processes/graph_tools.py
@@ -56,3 +56,54 @@ def find_stationary_matrices(matrix: numpy.ndarray, num_alternatives = 1, verbos
     if num_alternatives == 1:
         return stationary_matrices[0]
     return stationary_matrices
+
+def get_optimal_strategy(p: np.ndarray, r: np.ndarray, k: int):
+    optimal = -np.ones(k, dtype=int)
+    kk = np.zeros(k, dtype=int)
+    v_k_i = np.sum(p * r, axis=2)
+
+    while np.any(kk != optimal):
+        optimal = kk
+
+        SLAU = np.zeros((k + 1, k + 1))
+        b = np.zeros(k + 1)
+        SLAU[:k, 0] = 1
+
+        for i in range(k + 1):
+            if i < k:
+                SLAU[i, 1:] = -p[kk[i], i, :]
+                SLAU[i, i+1] += 1
+                b[i] = v_k_i[kk[i], i]
+            else:
+                SLAU[i, i] = 1
+                b[i] = 0
+
+        coefs = np.linalg.solve(SLAU, b)
+
+        f_s = coefs[1:]
+        kk = np.argmax(np.sum(p * r, axis=2, keepdims=True) + np.sum(p * f_s, axis=2, keepdims=True), axis=0)
+
+    return optimal, coefs[0]
+
+def get_optimal_discounted_strategy(p: np.ndarray, r: np.ndarray, k: int, beta: float):
+    optimal = -np.ones(k, dtype=int)
+    kk = np.zeros(k, dtype=int)
+    v_k_i = np.sum(p * r, axis=2)
+
+    while np.any(kk != optimal):
+        optimal = kk
+
+        SLAU = np.zeros((k, k))
+        b = np.zeros(k )
+
+        for i in range(k):
+
+            SLAU[i, :] = -beta * p[kk[i], i, :]
+            SLAU[i, i] += 1
+            b[i] = v_k_i[kk[i], i]
+
+        coefs = np.linalg.solve(SLAU, b)
+
+        kk = np.argmax(np.sum(p * r, axis=2, keepdims=True) + beta * np.sum(p * coefs, axis=2, keepdims=True), axis=0)
+
+    return optimal
